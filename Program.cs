6427f3e2eb5211b4e098ad3422c1bedc00c05196
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-01 19:36:01 -0300

    Eu amo medir km

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double milhas;
+double km;
+
+Console.Clear();
+
+Console.Write("Entre com a medida (em milhas): ");
+milhas = Convert.ToDouble (Console.ReadLine());
+
+km = milhas * 1.609;
+
+Console.WriteLine($"Medida em KM: {km} ");
